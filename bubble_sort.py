69ee70041ef4f0e13b4aed27499fb495bf3022fa
shmulisarmy <shmulikeller@gmail.com>
2023-11-19 22:33:28 -0500

    ..

---
+++ bubble_sort.py
@@ -0,0 +1,36 @@
+import pygame, random
+
+def handle_display():
+    window.fill('black')
+
+    for i, num in enumerate(numbers):
+        pygame.draw.rect(window, (255, 255, 255), pygame.Rect(PILLAR_WIDTH*i, 0, PILLAR_WIDTH, PILLAR_HEIGHT*num))
+
+    pygame.display.update()
+
+pygame.init()
+WIDTH, HEIGHT = 800, 800
+MAX_NUM_AMOUNT = 60
+FPS = MAX_NUM_AMOUNT//5
+numbers = [random.randint(1, 60) for i in range(random.randint(5, MAX_NUM_AMOUNT))]
+PILLAR_WIDTH, PILLAR_HEIGHT = WIDTH/len(numbers), HEIGHT/max(numbers)
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+for i in range(len(numbers)-1, -1, -1):
+    for j in range(1, i+1):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        if numbers[j-1] > numbers[j]:
+            clock.tick(FPS)
+            numbers[j], numbers[j-1] = numbers[j-1], numbers[j]
+            handle_display()
+
+while True:
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+    handle_display()
